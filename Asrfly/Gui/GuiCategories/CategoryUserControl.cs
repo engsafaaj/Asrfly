b0fae50d86bc06082734bb917c71a38749d7756a
tecno1001 <utecno76@gmail.com>
2023-02-04 11:07:57 +0300

    Add Categores Page

---
+++ Asrfly/Gui/GuiCategories/CategoryUserControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Asrfly.Gui.GuiCategories
+{
+    public partial class CategoryUserControl : UserControl
+    {
+        public CategoryUserControl()
+        {
+            InitializeComponent();
+        }
+        #region Events
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
