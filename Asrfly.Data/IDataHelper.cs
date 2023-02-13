92ffd08d469f1b06df1e382d582029dc31cff3cb
tecno1001 <utecno76@gmail.com>
2023-02-13 20:14:56 +0300

    Add Code Doc of IDataHelper

--- Asrfly.Data/IDataHelper.cs
+++ Asrfly.Data/IDataHelper.cs
@@ -8,23 +8,59 @@ namespace Asrfly.Data
     public interface IDataHelper<Table>
     {
         // Read
+        /// <summary>Gets all data.</summary>
+        /// <returns>
+        ///   <br />
+        /// </returns>
         List<Table> GetAllData();
+        /// <summary>Searches the specified search item.</summary>
+        /// <param name="SearchItem">The search item.</param>
+        /// <returns>Searched Data</returns>
         List<Table> Search(string SearchItem);
+        /// <summary>Finds the specified identifier.</summary>
+        /// <param name="Id">The identifier.</param>
+        /// <returns>Row Table When Id ==Id</returns>
         Table Find(int Id);
 
         // Write
+        /// <summary>Adds the specified table.</summary>
+        /// <param name="table">The table.</param>
+        /// <returns>1 when the task success, 0 when task failed</returns>
         int Add(Table table);
+        /// <summary>Edits the specified table.</summary>
+        /// <param name="table">The table.</param>
+        /// <returns>1 when the task success, 0 when task failed</returns>
         int Edit(Table table);
+        /// <summary>Deletes the specified identifier.</summary>
+        /// <param name="Id">The identifier.</param>
+        /// <returns>1 when the task success, 0 when task failed</returns>
         int Delete(int Id);
 
         // Read Async
+        /// <summary>Gets all data asynchronous.</summary>
+        /// <returns>All Data Of Table</returns>
         Task<List<Table>> GetAllDataAsync();
+        /// <summary>Searches the asynchronous.</summary>
+        /// <param name="SearchItem">The search item.</param>
+        /// <returns>Row Data</returns>
         Task<List<Table>> SearchAsync(string SearchItem);
+        /// <summary>Finds the asynchronous.</summary>
+        /// <param name="Id">The identifier.</param>
+        /// <returns>Row Data</returns>
         Task<Table> FindAsync(int Id);
 
         // Write Async
+        /// <summary>Adds the asynchronous.</summary>
+        /// <param name="table">The table.</param>
+        /// <returns>1 when the task success, 0 when task failed</returns>
         Task<int> AddAsync(Table table);
+        /// <summary>Edits the asynchronous.</summary>
+        /// <param name="table">The table.</param>
+        /// <returns>1 when the task success, 0 when task failed</returns>
         Task<int> EditAsync(Table table);
+        /// <summary>Deletes the asynchronous.</summary>
+        /// <param name="Id">The identifier.</param>
+        /// <returns>1 when the task success, 0 when task failed</returns>
         Task<int> DeleteAsync(int Id);
     }
 }
