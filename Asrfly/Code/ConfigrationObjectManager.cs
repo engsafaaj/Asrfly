d0a187fe89437f00fc696b74965f796296514b33
tecno1001 <utecno76@gmail.com>
2023-02-17 19:53:09 +0300

    Add Dependency Injection

---
+++ Asrfly/Code/ConfigrationObjectManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asrfly.Code
+{
+   public static class ConfigrationObjectManager
+    {
+        private static Dictionary<string, object> ObjectList = new Dictionary<string, object>();
+
+        public static void Register(string ObjectName,object ObjectValue)
+        {
+            ObjectList.Add(ObjectName, ObjectValue);
+        }
+
+        public static object GetObject(string ObjectName)
+        {
+            return ObjectList[ObjectName];
+        }
+    }
+}
---
+++ Asrfly/Code/DependnecyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Asrfly.Data.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asrfly.Code
+{
+   public static class DependnecyInjection
+    {
+        public static void AddDependencyValues()
+        {
+            ConfigrationObjectManager.Register("Categories", new CategoriesEntity());
+        }
+    }
+}
